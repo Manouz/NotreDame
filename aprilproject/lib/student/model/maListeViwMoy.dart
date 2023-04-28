6a53c47dff0893b5f0135044be8b3b0ce99a0692
Franck Obité <obitefrank@gmail.com>
2023-04-28 14:08:32 +0000

    2 commit obité

--- aprilproject/lib/student/model/maListeViwMoy.dart
+++ aprilproject/lib/student/model/maListeViwMoy.dart
@@ -39,81 +39,81 @@ class MaListViewMoyenne extends StatelessWidget {
             "Fancais",
             style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
           )),
-          DataCell(Text("...")),
-          DataCell(Text("...")),
-          DataCell(Text("..."))
+          DataCell(Text("10, 12, 15")),
+          DataCell(Text(" ${47 / 3}")),
+          DataCell(Text("5"))
         ]),
         DataRow(cells: [
           DataCell(Text(
             "Histoire-Géo",
             style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
           )),
-          DataCell(Text("...")),
-          DataCell(Text("...")),
-          DataCell(Text("..."))
+          DataCell(Text("02, 12, 8")),
+          DataCell(Text("${(02 + 12 + 8) / 3}")),
+          DataCell(Text("10"))
         ]),
         DataRow(cells: [
           DataCell(Text(
             "Anglais",
             style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
           )),
-          DataCell(Text("...")),
-          DataCell(Text("...")),
-          DataCell(Text("..."))
+          DataCell(Text("12, 12, 18")),
+          DataCell(Text("${(12 + 12 + 18) / 3}")),
+          DataCell(Text("10"))
         ]),
         DataRow(cells: [
           DataCell(Text(
             "Maths",
             style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
           )),
-          DataCell(Text("...")),
-          DataCell(Text("...")),
-          DataCell(Text("..."))
+          DataCell(Text("12, 12, 18")),
+          DataCell(Text("${(12 + 12 + 18) / 3}")),
+          DataCell(Text("10"))
         ]),
         DataRow(cells: [
           DataCell(Text(
             "SVT",
             style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
           )),
-          DataCell(Text("...")),
-          DataCell(Text("...")),
-          DataCell(Text("..."))
+          DataCell(Text("17, 12, 18")),
+          DataCell(Text("${(17 + 12 + 18) / 3}")),
+          DataCell(Text("3"))
         ]),
         DataRow(cells: [
           DataCell(Text(
             "Physique-Ch",
             style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
           )),
-          DataCell(Text("...")),
-          DataCell(Text("...")),
-          DataCell(Text("..."))
+          DataCell(Text("1, 12, 18")),
+          DataCell(Text("${(1 + 12 + 18) / 3}")),
+          DataCell(Text("6"))
         ]),
         DataRow(cells: [
           DataCell(Text(
             "Philosophie",
             style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
           )),
-          DataCell(Text("...")),
-          DataCell(Text("...")),
-          DataCell(Text("..."))
+          DataCell(Text("12, 12, 18")),
+          DataCell(Text("${(12 + 12 + 18) / 3}")),
+          DataCell(Text("10"))
         ]),
         DataRow(cells: [
           DataCell(Text(
             "EPS",
             style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
           )),
-          DataCell(Text("...")),
-          DataCell(Text("...")),
-          DataCell(Text("..."))
+          DataCell(Text("12, 2, 1")),
+          DataCell(Text("${(12 + 2 + 1) / 3}")),
+          DataCell(Text("25"))
         ]),
         DataRow(cells: [
           DataCell(Text(
             "Conduite",
             style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
           )),
-          DataCell(Text("...")),
-          DataCell(Text("...")),
-          DataCell(Text("..."))
+          DataCell(Text("12, 12, 18")),
+          DataCell(Text("${(12 + 12 + 18) / 3}")),
+          DataCell(Text("10"))
         ])
       ]),
     ]);
